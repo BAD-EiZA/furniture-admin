ac314daf5429b385c8299abceada13a17e1ff4db
BAD-EiZA <badapplestd@gmail.com>
2026-04-06 01:15:53 +0700

    [Erza] Update Data

--- src/components/cart-badge.tsx
+++ src/components/cart-badge.tsx
@@ -29,10 +29,10 @@ export default function CartBadge() {
             {open ? (
                 <>
                     <div
-                        className="fixed inset-0 z-30"
+                        className="fixed inset-0 z-50"
                         onClick={() => setOpen(false)}
                     />
-                    <div className="absolute right-0 top-14 z-40">
+                    <div className="absolute right-0 top-14 z-50">
                         <MiniCartPreview />
                     </div>
                 </>
